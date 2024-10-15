5471f856c3d13550e530bb92611a80de250ba750
Kinuthia <kamalijosephat032@gmail.com>
2024-10-15 16:41:36 +0300

    revamp the hero section

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import CustomCarousel from './CustomCarousel';
 import heroContent from './heroContent';
+import Homecard from '../../pages/homeCard';
 
 const Hero = ({ className }) => {
-  return (
-    <div className={`hero ${className} bg-light-grey`}>
-      <CustomCarousel
+  return (<>
+    <div className={`hero ${className} bg-light-greyflex flex-col items-center`}>
+      <CustomCarousel 
         images={heroContent.images}
         title={heroContent.title}
         subtitle={heroContent.subtitle}
       />
+      <div className="w-full max-w-4xl  mt-80 px-4 sm:px-6 lg:px-8">
+        <Homecard />
+      </div>
     </div>
+    </>
   );
 }
 
